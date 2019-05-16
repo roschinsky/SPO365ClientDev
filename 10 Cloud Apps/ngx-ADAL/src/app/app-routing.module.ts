e83fd4f22bbc29c1965c461ca1bfcbd40e73b838
Alexander Pajer <alexander.pajer@integrations.at>
2019-05-16 13:38:17 +0200

    ng fix

--- 10 Cloud Apps/ngx-ADAL/src/app/app-routing.module.ts
+++ 10 Cloud Apps/ngx-ADAL/src/app/app-routing.module.ts
@@ -4,14 +4,13 @@ import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { AdalGuard } from 'adal-angular4';
 
-
 const routes: Routes = [
-  { path: '', component: HomeComponent, canActivate: [AdalGuard] }
-  { path: '**', component: NotFoundComponent }
+	{ path: '', component: HomeComponent, canActivate: [ AdalGuard ] },
+	{ path: '**', component: NotFoundComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [ RouterModule.forRoot(routes) ],
+	exports: [ RouterModule ]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- 10 Cloud Apps/ngx-ADAL/src/environments/environment.ts
+++ 10 Cloud Apps/ngx-ADAL/src/environments/environment.ts
@@ -1,13 +1,13 @@
 export const environment = {
-  production: false,
-  config: {
-    tenant: "d92b247e-90e0-4469-a129-6a32866c0d0a",
-    clientId: "4e60c128-a813-4031-bd99-cf4327cce885", //=> Application ID in Azure
-    cacheLocation: "localStorage",
-    endpoints: {
-      graphApiUri: "https://graph.microsoft.com",
-      sharePointUri: "https://integrationsonline.sharepoint.com" // Replace with your Tenant
-    },
-    returnUrl: "http://localhost:5000"
-  }
+	production: false,
+	config: {
+		tenant: 'd92b247e-90e0-4469-a129-6a32866c0d0a',
+		clientId: '4e60c128-a813-4031-bd99-cf4327cce885', //=> Application ID in Azure
+		cacheLocation: 'localStorage',
+		endpoints: {
+			graphApiUri: 'https://graph.microsoft.com',
+			sharePointUri: 'https://integrationsonline.sharepoint.com' // Replace with your Tenant
+		},
+		returnUrl: 'http://localhost:4200'
+	}
 };
