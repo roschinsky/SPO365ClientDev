7fd8eacd5b1e3b09874af910ddf962d0057943e1
Thomas Roschinsky <roschinsky@hotmail.com>
2020-01-14 10:15:00 +0100

    Added WSP template

---
+++ Smpl/SLN_Template/SLN_Template/Layouts/SLN_Template/ApplicationPage1.aspx.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.WebControls;
+
+namespace SLN_Template.Layouts.SLN_Template
+{
+    public partial class ApplicationPage1 : LayoutsPageBase
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
